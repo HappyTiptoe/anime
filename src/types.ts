02c17070c0ce9120f4b37869d619ee9560bdc8df
ebxn <eb3ntuff@gmail.com>
2021-06-26 16:11:13 +0100

    fix: remove type 'easing' from config/options

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,13 @@
 export type Fn = (...params: any[]) => any
 
-// prettier-ignore
-export type AnimeTimingFunction = 'linear' | 'ease' | 'ease-in' | 'ease-out' | 'ease-in-out' | 'initial' | 'inherit'
-
 export type AnimeConfig = {
   duration?: number
-  easing?: AnimeTimingFunction
   delay?: number
   offset?: number
 }
 
 export type AnimeOptions = {
   duration: number
-  easing: AnimeTimingFunction
   delay: number
   offset: number
 }
